9a573e4e9fc31ecd156458e99a2a1397e38c2303
Jens Hadlich <jenshadlich@googlemail.com>
2016-02-02 18:00:03 +0100

    split up: server, route, routes

---
+++ rest/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+type Route struct {
+    Method      string
+    Pattern     string
+    HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+    Route{
+        "GET", "/hello", Hello,
+    },
+}
--- rest/server.go
+++ rest/server.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
     "log"
     "net/http"
     "time"
 )
 
 func main() {
-    requestRouter := mux.NewRouter().StrictSlash(true)
-    requestRouter.HandleFunc("/hello", Hello).Methods("GET")
-
     server := &http.Server{
         Addr:           ":8000",
-        Handler:        requestRouter,
+        Handler:        NewRouter(),
         ReadTimeout:    10 * time.Second,
         WriteTimeout:   10 * time.Second,
         MaxHeaderBytes: 1 << 13, // 8K
